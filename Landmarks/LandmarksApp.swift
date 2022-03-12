7cbdde44cc308b666d39edfdd1ceb7c235ab4b07
mocchan <hcmnorgir0808@yahoo.co.jp>
2022-03-12 14:59:05 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 岩本康孝 on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
